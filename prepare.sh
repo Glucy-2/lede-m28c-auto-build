0305adf44ae4d224d7867bd3e4f5e74d50abe059
Glucy2 <glucy-2@outlook.com>
2025-08-01 20:10:12 +0800

    feat: update luci to 24.10

--- prepare.sh
+++ prepare.sh
@@ -16,6 +16,7 @@ fi
 
 #cat ../m28c.config > .config
 cat feeds.conf.default > feeds.conf
+sed -i 's#https://github.com/coolsnowwolf/luci.git;openwrt-23.05#https://github.com/coolsnowwolf/luci.git;openwrt-24.10#g' feeds.conf
 echo -e "\nsrc-git qmodem https://github.com/FUjr/QModem.git;main" >> feeds.conf
 rm -rf files
 cp -r ../files .
