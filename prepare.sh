378b74e2e4ed574aa0b18fc58e302ddb67bf8f0c
Glucy2 <glucy-2@outlook.com>
2025-08-02 13:56:22 +0800

    fix: change default feeds

--- prepare.sh
+++ prepare.sh
@@ -15,8 +15,8 @@ else
 fi
 
 #cat ../m28c.config > .config
+sed -i 's#https://github.com/coolsnowwolf/luci.git;openwrt-23.05#https://github.com/coolsnowwolf/luci.git;openwrt-24.10#g' feeds.conf.default
 cat feeds.conf.default > feeds.conf
-sed -i 's#https://github.com/coolsnowwolf/luci.git;openwrt-23.05#https://github.com/coolsnowwolf/luci.git;openwrt-24.10#g' feeds.conf
 echo -e "\nsrc-git qmodem https://github.com/FUjr/QModem.git;main" >> feeds.conf
 rm -rf files
 cp -r ../files .
